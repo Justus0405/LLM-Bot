9597f984eed9f69344dff5072374e71b5962634c
Justus0405 <justus040506@gmail.com>
2025-10-13 10:38:37 +0200

    Improved build speed

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-clear && docker-compose down && docker-compose up -d --build && docker logs --follow llm-bot
+clear && docker-compose down --timeout 0 && docker-compose up -d --build && docker logs --follow llm-bot
