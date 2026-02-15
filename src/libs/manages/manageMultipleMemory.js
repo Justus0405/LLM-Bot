a0816f9a04adf6e941ad86fa3bef7195213f399b
Justus0405 <justus040506@gmail.com>
2026-02-15 21:07:23 +0100

    Fixed llama.cpp Exception: Conversation roles must alternate user/assistant/user/assistant/...

--- src/libs/manages/manageMultipleMemory.js
+++ src/libs/manages/manageMultipleMemory.js
@@ -3,6 +3,13 @@ async function manageMultipleMemory(client, message, conversation) {
     // Get the last 20 messages.
     let prevMessages = await message.channel.messages.fetch({ limit: 20 });
 
+    // Use alternating user and assistant role which is required by some APIs.
+    // NOTE:
+    // This causes for some messages to be ignored in history,
+    // currently older messages are prioritised over newer ones,
+    // maybe I should reverse the entries first when I have filtered them.
+    let prevRole = 'none'
+
     // Reverse because discord returns the latest message first.
     prevMessages.reverse();
 
@@ -17,10 +24,18 @@ async function manageMultipleMemory(client, message, conversation) {
             // Ignore replies that dont belong to the current conversation of the user.
             if (!msg.mentions.users.has(message.author.id)) return;
 
+            // If the previous message is also a assistant message discard it.
+            // This fixes llamacpp error:
+            // Error: Jinja Exception: Conversation roles must alternate user/assistant/user/assistant/...
+            if (prevRole !== 'user' && prevRole !== 'none') return;
+
             conversation.push({
                 role: 'assistant',
                 content: msg.content,
             });
+
+            // Set the variable to assistant for allowing the next processed message to be a user one.
+            prevRole = 'assistant'
 
             return;
         }
@@ -32,6 +47,11 @@ async function manageMultipleMemory(client, message, conversation) {
             // These messages dont count as part of the conversation.
             if (!msg.mentions.users.has(client.user.id)) return;
 
+            // If the previous message is also a user message discard it.
+            // This fixes llamacpp error:
+            // Error: Jinja Exception: Conversation roles must alternate user/assistant/user/assistant/...
+            if (prevRole !== 'assistant' && prevRole !== 'none') return;
+
             // Remove the id of the bot.
             // Converts "@BOT_ID Hello!" -> "Hello!".
             const prompt = msg.content.replace(`<@${client.user.id}>`, '').trim();
@@ -40,6 +60,9 @@ async function manageMultipleMemory(client, message, conversation) {
                 role: 'user',
                 content: prompt,
             });
+
+            // Set the variable to user for allowing the next processed message to be a assistant one.
+            prevRole = 'user'
 
             return;
         }
